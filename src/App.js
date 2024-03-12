082c9774eab32e191f4d80354d129b2e1b0eac9b
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-03-12 17:12:26 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-500 flex justify-center py-10">
+      <div className=" border-8 border-red-600 w-10/12">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
